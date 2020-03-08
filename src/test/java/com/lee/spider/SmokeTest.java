f4c9fa65fff1fd5924944c77c4afd6ada070809d
Lee <lizhijie_lee@126.com>
2020-03-08 17:25:20 +0800

    firstCommit

---
+++ src/test/java/com/lee/spider/SmokeTest.java
@@ -0,0 +1,15 @@
+package com.lee.spider;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Description:
+ * User: Sujing
+ * Date: 2020-03-08
+ * Time: 17:21
+ */
+public class SmokeTest {
+    @Test
+    public void test() {
+    }
+}
