3c5e4b025c5747f1e06ad94a7a960e13c76f22c1
Lee <lizhijie_lee@126.com>
2020-03-12 12:25:52 +0800

    压制了spotBugs的一个可能的NP检测

--- src/main/java/com/lee/spider/SearchDemo.java
+++ src/main/java/com/lee/spider/SearchDemo.java
@@ -1,19 +1,19 @@
 package com.lee.spider;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.common.document.DocumentField;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 import java.io.IOException;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 /**
@@ -22,8 +22,9 @@ import java.util.Scanner;
  * Date: 2020-03-12
  * Time: 10:44
  */
+@SuppressFBWarnings("NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE")
 public class SearchDemo {
-    private static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in, String.valueOf(StandardCharsets.UTF_8));
 
     public static void main(String[] args) throws IOException {
         while (true) {
@@ -32,21 +33,25 @@ public class SearchDemo {
             if ("%exit%".equals(keyWord)) {
                 System.exit(-1);
             }
-            try (RestHighLevelClient client = new RestHighLevelClient(
-                    RestClient.builder(new HttpHost("localhost", 9200, "http")))) {
-                SearchRequest searchRequest = new SearchRequest("news");
+            searchFromElasticSearch(keyWord);
+        }
+    }
 
-                QueryBuilder matchQueryBuilder = QueryBuilders.multiMatchQuery(keyWord, "title", "content");
-                SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-                sourceBuilder.query(matchQueryBuilder);
-                searchRequest.source(sourceBuilder);
-                SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
-                String hits = response.getHits().getTotalHits().toString();
-                System.out.println(hits);
-                for (SearchHit hit : response.getHits().getHits()) {
-                    String sourceAsString = hit.getSourceAsString();
-                    System.out.println(sourceAsString);
-                }
+    private static void searchFromElasticSearch(String keyWord) throws IOException {
+        try (RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http")))) {
+            SearchRequest searchRequest = new SearchRequest("news");
+
+            QueryBuilder matchQueryBuilder = QueryBuilders.multiMatchQuery(keyWord, "title", "content");
+            SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+            sourceBuilder.query(matchQueryBuilder);
+            searchRequest.source(sourceBuilder);
+            SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+            String hits = response.getHits().getTotalHits().toString();
+            System.out.println(hits);
+            for (SearchHit hit : response.getHits().getHits()) {
+                String sourceAsString = hit.getSourceAsString();
+                System.out.println(sourceAsString);
             }
         }
     }
