7868909c60414430569eadbe0b17bf62b8f17675
Lee <lizhijie_lee@126.com>
2020-03-12 12:00:18 +0800

    项目基本完成 实现了简单的CI功能，其实就是检查语法 利用spotBugs进行bug检查 能够从一个页面进行页面爬取 只爬取感兴趣的news部分，很粗糙的过滤 利用数据库储存待处理和处理过的url 能够使用多线程mock百万级数据插入数据库 git commit -m 项目基本完成

--- src/main/java/com/lee/spider/SearchDemo.java
+++ src/main/java/com/lee/spider/SearchDemo.java
@@ -1,5 +1,21 @@
 package com.lee.spider;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.document.DocumentField;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * Description:
  * User: Lzj
@@ -7,4 +23,31 @@ package com.lee.spider;
  * Time: 10:44
  */
 public class SearchDemo {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            System.out.println("请输入标题进行搜索（%exit%退出程序）");
+            String keyWord = scanner.next();
+            if ("%exit%".equals(keyWord)) {
+                System.exit(-1);
+            }
+            try (RestHighLevelClient client = new RestHighLevelClient(
+                    RestClient.builder(new HttpHost("localhost", 9200, "http")))) {
+                SearchRequest searchRequest = new SearchRequest("news");
+
+                QueryBuilder matchQueryBuilder = QueryBuilders.multiMatchQuery(keyWord, "title", "content");
+                SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+                sourceBuilder.query(matchQueryBuilder);
+                searchRequest.source(sourceBuilder);
+                SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+                String hits = response.getHits().getTotalHits().toString();
+                System.out.println(hits);
+                for (SearchHit hit : response.getHits().getHits()) {
+                    String sourceAsString = hit.getSourceAsString();
+                    System.out.println(sourceAsString);
+                }
+            }
+        }
+    }
 }
